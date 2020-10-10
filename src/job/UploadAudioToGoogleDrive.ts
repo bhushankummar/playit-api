07ded5ae70f00265780b8ea5bf9c974cbe5d1b0f
BL <bhushankumar.lilapara@gmail.com>
2020-10-10 15:49:01 +0530

    removes cron job to use the emails

--- src/job/UploadAudioToGoogleDrive.ts
+++ src/job/UploadAudioToGoogleDrive.ts
@@ -1,6 +1,6 @@
 import * as Debug from 'debug';
 import * as cron from 'cron';
-import {APP, CRONE_JOB, ENDPOINT, UPLOAD_AUDIO_SCHEDULE} from '../constants';
+import { APP, CRONE_JOB, ENDPOINT, UPLOAD_AUDIO_SCHEDULE } from '../constants';
 import * as _ from 'lodash';
 import * as request from 'request-promise';
 
@@ -10,11 +10,11 @@ const debug = Debug('PL:JOB-UploadAudioToGoogleDrive');
 export const init: any = () => {
     /*jshint  -W031 : false */
     const croneTime = UPLOAD_AUDIO_SCHEDULE.Seconds + ' ' +
-      UPLOAD_AUDIO_SCHEDULE.Minutes + ' ' +
-      UPLOAD_AUDIO_SCHEDULE.Hours + ' ' +
-      UPLOAD_AUDIO_SCHEDULE.DayOfMonth + ' ' +
-      UPLOAD_AUDIO_SCHEDULE.Months + ' ' +
-      UPLOAD_AUDIO_SCHEDULE.DayOfWeek;
+        UPLOAD_AUDIO_SCHEDULE.Minutes + ' ' +
+        UPLOAD_AUDIO_SCHEDULE.Hours + ' ' +
+        UPLOAD_AUDIO_SCHEDULE.DayOfMonth + ' ' +
+        UPLOAD_AUDIO_SCHEDULE.Months + ' ' +
+        UPLOAD_AUDIO_SCHEDULE.DayOfWeek;
     const job = new CronJob(croneTime, start, undefined, false, CRONE_JOB.TIMEZONE);
     debug('.............. UploadAudioToGoogleDrive Job Initiated Successfully, still you have to execute start() ........');
     return job;
@@ -28,28 +28,20 @@ const start: any = async () => {
     }
     // debug('.............. Start ........');
     taskRunning = true;
-    const users = APP.ALLOWED_EMAILS;
-    await Promise.all(users.map(async (email: string) => {
-        if (_.isEmpty(email)) {
-            return;
-        }
-        const data = {
-            email: email
-        };
-        const options = {
-            method: 'POST',
-            uri: `${ENDPOINT.UPLOAD}/audio`,
-            body: data,
-            json: true
-        };
 
-        try {
-            await request(options);
-        } catch (error) {
-            // debug('error ', error);
-        } finally {
+    const options = {
+        method: 'POST',
+        uri: `${ENDPOINT.UPLOAD}/audio`,
+        body: {},
+        json: true
+    };
 
-        }
-    }));
+    try {
+        await request(options);
+    } catch (error) {
+        // debug('error ', error);
+    } finally {
+
+    }
     taskRunning = false;
 };
