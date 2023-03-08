047e36963ff3b5b19eaff614b3e546b16102abd1
BL <bhushankumar.lilapara@gmail.com>
2023-03-08 21:17:10 +0530

    remove unused packages and add the config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,10 +7,24 @@ import { MediaItemModule } from './media-item/media-item.module';
 import { PlaylistModule } from './playlist/playlist.module';
 import { YoutubeModule } from './youtube/youtube.module';
 import { GoogleDriveModule } from './google-drive/google-drive.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-    imports: [UserModule, TokenModule, MediaItemModule, PlaylistModule, YoutubeModule, GoogleDriveModule],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            envFilePath: [`env.${process.env.NODE_ENV}.env`],
+            expandVariables: true,
+        }),
+        UserModule,
+        TokenModule,
+        MediaItemModule,
+        PlaylistModule,
+        YoutubeModule,
+        GoogleDriveModule,
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
+
 export class AppModule { }
